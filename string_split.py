ccc54ab14aa558508fc38026f626a715597f17f5
kanchanasujan <125042603+kanchanasujan@users.noreply.github.com>
2025-02-07 12:39:51 +0530

    Add Python files

---
+++ string_split.py
@@ -0,0 +1,16 @@
+text = "I deserve all happiness and prosperity in life"
+text1 = text.split()
+print(text1)
+
+
+data = "I deserve all happiness and prosperity in life"
+data1 = text.split("and")
+print(data1)
+
+sentence = "I deserve all happiness and prosperity in life"
+words = text.split("in")
+print(words)
+
+str = "I deserve all happiness and prosperity in life"
+words = str[2:10]
+print(words)
---
+++ string_strip.py
@@ -0,0 +1,5 @@
+text = "   Some spaces around   "
+stripped_text = text.strip()
+stripped_text1 = text.strip("spaces")
+print("Stripped text:", stripped_text)
+print("Stripped text1:", stripped_text1)
